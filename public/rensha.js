fd2b390bdc2671e6a1b7de245ed1b3264b500aaf
HidehikoKondo <dev@udonko.net>
2020-01-01 21:49:57 +0900

    ちょっとリファクタ

--- public/rensha.js
+++ public/rensha.js
@@ -49,8 +49,7 @@ function startTitleScene() {
     start.tl.scaleTo(0.8, 0);
     game.rootScene.addChild(start);
     start.on('touchstart', function (e) {
-        var sound = game.assets['sounds/start.mp3'].clone();
-        sound.play();
+        playSE('sounds/start.mp3');
         this.tl.scaleTo(0.9, 0.9, 3);
         this.tl.delay(8);
         this.tl.then(function () {
@@ -66,8 +65,7 @@ function startTitleScene() {
     ranking.tl.scaleTo(0.6, 0);
     game.rootScene.addChild(ranking);
     ranking.on('touchstart', function (e) {
-        var sound = game.assets['sounds/start.mp3'].clone();
-        sound.play();
+        playSE('sounds/start.mp3');
         this.tl.scaleTo(0.7, 0.7, 3);
         this.tl.delay(8);
         alert("工事中");
@@ -93,6 +91,15 @@ function startGameScene(tap) {
     var combo = 1;
     var gameoverFlg = false;
     var anger = 1;
+
+
+    //ハイスコア
+    var highScore = localStorage.getItem("tlh-highscore");
+    if (!highScore) {
+        highScore = 0;
+    }
+    console.log(highScore);
+    localStorage.setItem("tlh-highscore", 100);
 
     //シーン作成
     var gameScene = new Scene();
@@ -193,8 +200,7 @@ function startGameScene(tap) {
                     label.color = "black";
                 }
                 playingFlg = true;
-                var sound = game.assets['sounds/miss.mp3'].clone();
-                sound.play();
+                playSE('sounds/miss.mp3');
             }
 
         }
@@ -205,8 +211,7 @@ function startGameScene(tap) {
         finger.tl.hide();
         if (nuitaFlg == false) {
             //抜けなかった
-            var sound = game.assets['sounds/unplug.mp3'].clone();
-            sound.play();
+            playSE('sounds/unplug.mp3');
             hair.x = (gameScene.width - hair.width) / 2;
             hair.y = 270;
             hair.tl.scaleTo(1, 1, 4);
@@ -259,64 +264,11 @@ function gameOverScene(count) {
     background.y = -380;
     gameoverScene.addChild(background);
 
-    var sound = game.assets['sounds/gameover.mp3'].clone();
-    sound.play();
-
-
-    // var black = new Sprite(640, 100);
-    // black.backgroundColor = "black";
-    // black.opacity = 0.5
-    // black.x = (gameoverScene.width - black.width) / 2;
-    // black.y = 180;
-    // gameoverScene.addChild(black);
-
+    playSE('sounds/gameover.mp3');
 
     //得点ラベル
-    var label = new Label();
-    label.width = 640;
-    label.height = 40;
-    label.textAlign = "center";
-    label.color = "black";
-    label.text = "今回の得点：" + count + "本抜き";
-    label.font = "30px 'Kosugi Maru'";
-    label.x = 2;
-    label.y = 192;
-    label.dropshadow
-    gameoverScene.addChild(label);
-
-    var label2 = new Label();
-    label2.width = 640;
-    label2.height = 40;
-    label2.textAlign = "center";
-    label2.color = "black";
-    label2.text = "ハイスコア：" + count + "本抜き";
-    label2.font = "30px 'Kosugi Maru'";
-    label2.x = 2;
-    label2.y = 242;
-    gameoverScene.addChild(label2);
-
-    //得点ラベル
-    var label3 = new Label();
-    label3.width = 640;
-    label3.height = 40;
-    label3.textAlign = "center";
-    label3.color = "white";
-    label3.text = "今回の得点：" + count + "本抜き";
-    label3.font = "30px 'Kosugi Maru'";
-    label3.x = 0;
-    label3.y = 190;
-    gameoverScene.addChild(label3);
-
-    var label4 = new Label();
-    label4.width = 640;
-    label4.height = 40;
-    label4.textAlign = "center";
-    label4.color = "white";
-    label4.text = "ハイスコア：" + count + "本抜き";
-    label4.font = "30px 'Kosugi Maru'";
-    label4.x = 0;
-    label4.y = 240;
-    gameoverScene.addChild(label4);
+    showScoreLabel(count, "今回の得点：", 0, 192, gameoverScene);
+    showScoreLabel(count, "ハイスコア：", 0, 240, gameoverScene);
 
     //ばかもん
     var bakamon = new Sprite(618, 125);
@@ -324,152 +276,53 @@ function gameOverScene(count) {
     bakamon.x = (gameoverScene.width - bakamon.width) / 2;
     bakamon.y = 300;
     bakamon.tl.scaleTo(0, 0, 1);
-    bakamon.tl.moveTo(0, 30, 30)
+    bakamon.tl.moveTo(0, 30, 60)
     bakamon.tl.and();
-    bakamon.tl.scaleTo(0.9, 0.9, 30);
+    bakamon.tl.scaleTo(0.9, 0.9, 60);
     gameoverScene.addChild(bakamon);
 
     //戻る
-    gameoverScene.on('touchend', function (e) {
-        var sound = game.assets['sounds/back.mp3'].clone();
-        sound.play();
-        //シーン遷移
-        game.replaceScene(game.rootScene);
-    });
-}
-
-function breakWaterMelon(gameScene) {
-    //PhyBoxSprite(幅, 高さ, 動作モード, 密度, 摩擦力, 反発力, フラグ)
-
-    //割れたスイカ
-    var phyBall = new PhyCircleSprite(116, enchant.box2d.DYNAMIC_SPRITE, 10.0, 1.0, 0, true);
-    phyBall.image = game.assets["images/watermelon-left.png"];
-    phyBall.position = { x: 390, y: 490 };
-    phyBall.angle = -10;
-    gameScene.addChild(phyBall); // シーンに追加
-
-    var phyBall2 = new PhyCircleSprite(116, enchant.box2d.DYNAMIC_SPRITE, 10.0, 1.0, 0, true);
-    phyBall2.image = game.assets["images/watermelon-right.png"];
-    phyBall2.position = { x: 410, y: 490 };
-    phyBall2.angle = 10;
-    gameScene.addChild(phyBall2); // シーンに追加
-
-    //床
-    var phyFloor = new PhyBoxSprite(400, 5, enchant.box2d.STATIC_SPRITE, 10, 1.0, 0, false);
-    phyFloor.x = 20;
-    phyFloor.y = 630;
-    //    phyFloor.backgroundColor = "red";
-    phyFloor.angle = -5;
-    gameScene.addChild(phyFloor);
-
-    var phyFloor3 = new PhyBoxSprite(400, 5, enchant.box2d.STATIC_SPRITE, 10, 1.0, 0, false);
-    phyFloor3.x = 20;
-    phyFloor3.y = 630;
-    //    phyFloor3.backgroundColor = "red";
-    phyFloor3.angle = 5;
-    gameScene.addChild(phyFloor3);
-
-    //床
-    var phyFloorR = new PhyBoxSprite(400, 5, enchant.box2d.STATIC_SPRITE, 10, 1.0, 0, false);
-    phyFloorR.x = 400;
-    phyFloorR.y = 630;
-    //    phyFloorR.backgroundColor = "red";
-    phyFloorR.angle = 5;
-    gameScene.addChild(phyFloorR);
-
-    //床
-    var phyFloorR = new PhyBoxSprite(400, 5, enchant.box2d.STATIC_SPRITE, 10, 1.0, 0, false);
-    phyFloorR.x = 400;
-    phyFloorR.y = 630;
-    //    phyFloorR.backgroundColor = "red";
-    phyFloorR.angle = -5;
-    gameScene.addChild(phyFloorR);
-
-}
-
-function gameClear(gameScene, meijin, item) {
-    //スイカ割り
-    item.remove();
-    breakWaterMelon(gameScene);
-
-    gameScene.tl.delay(300);
-    gameScene.tl.then(function () {
-        game.replaceScene(game.rootScene);
-    })
-
-
-    //プレイヤー
-    meijin.image = game.assets["images/meijin_hit.png"];
-
-    gameScene.touchEnabled = false;
-    var explosion = new Sprite(640, 480);
-    explosion.image = game.assets["images/explosion.png"];
-    explosion.tl.scaleTo(0.8, 0.8, 0);
-    explosion.rotation = -90;
-    explosion.x = 90;
-    explosion.y = 250;
-    gameScene.addChild(explosion);
-    var sound = game.assets['se/explosion.mp3'].clone();
-    sound.play();
-    explosion.frame = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18, 19, 19, null];
-    explosion.addEventListener(Event.ANIMATION_END, function () {
-        this.remove();
-
-        var clear = new Sprite(640, 250);
-        clear.image = game.assets["images/stageclear.png"];
-        clear.x = 640;
-        clear.y = 300;
-        gameScene.addChild(clear);
-        clear.tl.delay(90);
-        clear.tl.then(function () {
-            var sound = game.assets['se/clear.mp3'].clone();
-            sound.play();
+    bakamon.tl.then(function (e) {
+        gameoverScene.on('touchend', function (e) {
+            playSE('sounds/back.mp3');
+            //シーン遷移
+            game.replaceScene(game.rootScene);
         });
-        clear.tl.moveTo(0, 340, 7);
     });
-
-    //    game.stop();
-
 
 }
 
-function cutin(gameScene, meijin, item) {
-    var sound = game.assets['se/cutin.mp3'].clone();
+function showScoreLabel(score, message, x, y, scene) {
+    var label = new Label();
+    label.width = 640;
+    label.height = 40;
+    label.textAlign = "center";
+    label.color = "black";
+    label.text = message + score + "本抜き";
+    label.font = "30px 'Kosugi Maru'";
+    label.x = x + 2;
+    label.y = y + 2;
+    scene.addChild(label);
+
+    var label2 = new Label();
+    label2.width = 640;
+    label2.height = 40;
+    label2.textAlign = "center";
+    label2.color = "white";
+    label2.text = message + score + "本抜き";
+    label2.font = "30px 'Kosugi Maru'";
+    label2.x = x;
+    label2.y = y;
+    scene.addChild(label2);
+}
+
+function playSE(path) {
+    var sound = game.assets[path].clone();
     sound.play();
-
-    var cutinGroup = new Group();
-    cutinGroup.x = 640;
-    cutinGroup.y = 0;
-    gameScene.addChild(cutinGroup);
-    cutinGroup.tl.moveTo(0, 0, 6);
-    cutinGroup.tl.delay(60);
-    cutinGroup.tl.then(function () {
-        cutinGroup.remove();
-        gameClear(gameScene, meijin, item);
-    });
-
-    var back = new Sprite(640, 240);
-    back.image = game.assets["images/cutin.png"];
-    back.frame = [0, 1, 2, 3];
-    back.tl.scaleTo(1.2, 1.288, 0);
-    back.rotation = -10;
-    back.x = 0;
-    back.y = 239;
-    cutinGroup.addChild(back);
-
-    var face = new Sprite(640, 518);
-    face.image = game.assets["images/cutin_face.png"];
-    face.tl.scaleTo(0.6, 0.6, 0);
-    face.rotation = -10;
-    face.x = 0;
-    face.y = 100;
-    cutinGroup.addChild(face);
-}
-
+};
 
 function comboAnim(scene) {
-    var sound = game.assets['sounds/combo.mp3'].clone();
-    sound.play();
+    playSE('sounds/combo.mp3');
 
     var comboImg = new Sprite(628, 136);
     comboImg.image = game.assets["images/umiheicombo.png"];
